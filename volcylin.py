44a86be0c812d761a111739561ef978535f1364a
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:44:13 +0600

    Delete volcylin.py

--- volcylin.py
+++
@@ -1,5 +0,0 @@
-radius = float(input("Enter the radius of cylinder: "))
-height = float(input("Enter the height of cylinder: "))
-import math
-volume = math.pi * ((radius**2) * height)
-print("The volume of the cylinder is %.1f" % volume)
