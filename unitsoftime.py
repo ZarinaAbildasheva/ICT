2245622ca6ce0635332b7caf3826f549206e5392
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:43:58 +0600

    Delete unitsoftime.py

--- unitsoftime.py
+++
@@ -1,4 +0,0 @@
-print("Enter the number of days, hours, minutes, seconds by space: ", end = "")
-days, hours, minutes, seconds = [int(i) for i in input().split()]
-res = (minutes*60) + seconds + (hours*3600) + (days*3600*24)
-print(res, "seconds passed")
