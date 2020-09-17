e64ea9d8fffc15d2296062f2569a621aaba3bfa0
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:37:16 +0600

    Delete areanvol.py

--- areanvol.py
+++
@@ -1,7 +0,0 @@
-r = input("Enter the radius of circle and sphere: ")
-#pi = 22/7
-import math
-area = math.pi*(int(r)**2)
-volume = (4*math.pi*(int(r)**3))/3
-print("The area of the sphere: ", area)
-print("The volume of the sphere: ", volume)
