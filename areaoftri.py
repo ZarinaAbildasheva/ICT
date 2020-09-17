0ef45c67919efa0782be58fe02a0fa2556c5b5a9
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:37:56 +0600

    Delete areaoftri.py

--- areaoftri.py
+++
@@ -1,4 +0,0 @@
-b = float(input("Enter the value of length of the base: "))
-h = float(input("Enter the value of height of the triangle: "))
-area = b*(h/2)
-print("The area of triangle equals to %.2f meters" %area)
