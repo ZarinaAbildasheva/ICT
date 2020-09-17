bd38bb9d2d0b01b9018a176ae672c318d741e1b8
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:41:57 +0600

    Delete igl.py

--- igl.py
+++
@@ -1,7 +0,0 @@
-p = float(input("Enter the pressure: "))
-v = float(input("Enter the volume of the gas: "))
-t = float(input("Enter the temperature: "))
-r = 8.314
-tem = t+273.15
-n = (p*v)/(r*tem)
-print("Amount of gas in moles equals to %.2f moles." %n)
