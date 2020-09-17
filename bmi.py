f01542acb0452668ec50d70164581bd13355830c
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:38:39 +0600

    Delete bmi.py

--- bmi.py
+++
@@ -1,4 +0,0 @@
-height = float(input("Enter your height in meters: "))
-weight = float(input("Enter your weight in kilograms: "))
-bmi= weight/(height**2)
-print("Your Body Mass Index equals to: %.2f" %bmi)
