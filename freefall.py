41a8db1002017785ae080986a60d197831baf287
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:40:56 +0600

    Delete freefall.py

--- freefall.py
+++
@@ -1,4 +0,0 @@
-height = float(input("Enter the height from which object is dropped(m): "))
-g = 9.8
-res = (2*g*height)**(1/2)
-print("Final velocity of the object when it hits the ground %.2f" % res)
