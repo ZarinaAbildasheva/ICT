e5514949948bf3de07a9c1361de9806b9955ed5a
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:42:38 +0600

    Delete roomarea.py

--- roomarea.py
+++
@@ -1,7 +0,0 @@
-print("this program will calculate area the your room by entered width and length.")
-print("Please enter the length of the room in meters: ", end="")
-length = float(input())
-print("Okay, now enter the width of the room also in meters: ", end="")
-width = float(input())
-area =length*width
-print("The area of the room: "+str(area)+" square meters.")
