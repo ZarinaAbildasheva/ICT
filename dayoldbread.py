b6cc8dfdffe6703dbf1e16d3918f149400cb5f18
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-09-18 01:40:01 +0600

    Delete dayoldbread.py

--- dayoldbread.py
+++
@@ -1,9 +0,0 @@
-price = 3.49
-p = 60
-n = int(input("Enter amount of bread loaves: "))
-ordinary = round((price*n),2)
-sale = round(((ordinary*60)/100), 2)
-discount = round((ordinary-sale), 2)
-print("Ordinary price:", ordinary)
-print("Sale:", sale)
-print("Discount:", discount)
