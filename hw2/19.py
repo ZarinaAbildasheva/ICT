341592bb25ea4921f1a76a5246ca0f107800bc79
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-10-01 14:10:57 +0600

    Update 19.py

--- hw2/19.py
+++ hw2/19.py
@@ -1,7 +1,8 @@
+m = int(input())
 n = [int(i) for i in input().split()]
 count = 0
-for i in range(len(n)):
-    for j in range(i+1, len(n)):
+for i in range(m):
+    for j in range(i+1, m):
         if n[i] == n[j]:
             count+=1
 print(count)
