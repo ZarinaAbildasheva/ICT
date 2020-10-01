fbe02a67db6a519a755092319904d3bb2d52a7b4
ZarinaAbildasheva <60805634+ZarinaAbildasheva@users.noreply.github.com>
2020-10-01 14:10:29 +0600

    Update 18.py

--- hw2/18.py
+++ hw2/18.py
@@ -1,5 +1,6 @@
+m = int(input())
 n = [int(i) for i in input().split()]
-first = n[len(n)-1]
+first = n[m-1]
 print(first, end = " ")
-for j in range(0, len(n)-1):
+for j in range(0, m-1):
     print(n[j], end = " ")
